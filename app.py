4e0673d4600abac6590dbff2444bf49146006a84
DiwanDev <dev.diwan1308@gmail.com>
2026-07-15 18:09:02 +0530

    change login page

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 from flask import Flask, render_template, request, session, redirect, url_for
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 app = Flask(__name__)
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=os.getenv("MYSQL_PASSWORD"),
+    database="flask_db"
+)
+
+if db.is_connected():
+    print("Connected to MySQL successfully!")
+
 app.secret_key = "mysecretkey"
 
 @app.route("/")
@@ -54,12 +71,21 @@ def login():
                 message="Password is required"
             )
 
-        if username == "Diwan" and password == "12345":
-            session["username"] = username
-            return render_template(
-                "login.html",
-                message=f"Welcome {username}"
-            )
+        cursor = db.cursor(dictionary=True)
+
+        cursor.execute(
+            "SELECT * FROM users WHERE username = %s AND password = %s",
+            (username, password)
+        )
+
+        user = cursor.fetchone()
+
+        cursor.close()
+
+        if user:
+            session["username"] = user["username"]
+
+            return redirect(url_for("dashboard"))
 
         return render_template(
             "login.html",
@@ -67,4 +93,65 @@ def login():
         )
 
     return render_template("login.html")
+@app.route("/test-db")
+def test_db():
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM students")
+
+    students = cursor.fetchall()
+
+    cursor.close()
+
+    return str(students)
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == "":
+            return render_template(
+                "signup.html",
+                message="Username is required"
+            )
+
+        if password == "":
+            return render_template(
+                "signup.html",
+                message="Password is required"
+            )
+
+        cursor = db.cursor(dictionary=True)
+
+        cursor.execute(
+            "SELECT * FROM users WHERE username = %s",
+            (username,)
+        )
+
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            cursor.close()
+
+            return render_template(
+                "signup.html",
+                message="Username already exists"
+            )
+
+        cursor.execute(
+            "INSERT INTO users (username, password) VALUES (%s, %s)",
+            (username, password)
+        )
+
+        db.commit()
+        cursor.close()
+
+        session["username"] = username
+
+        return redirect(url_for("dashboard"))
+
+    return render_template("signup.html")
 app.run(debug=True)
